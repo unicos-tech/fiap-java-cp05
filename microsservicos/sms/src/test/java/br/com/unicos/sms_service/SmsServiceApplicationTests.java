0ab97a66ee147703b2c832f91a038f31acb1ce48
Ribaas <ribasleonardo92@gmail.com>
2025-10-21 19:43:42 -0300

    feat: scaffold do projeto de sms

---
+++ microsservicos/sms/src/test/java/br/com/unicos/sms_service/SmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unicos.sms_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
