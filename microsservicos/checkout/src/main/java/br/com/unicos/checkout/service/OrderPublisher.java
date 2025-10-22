c3956bca0f2a156af81eb7b37863d014e33152f2
Ribaas <ribasleonardo92@gmail.com>
2025-10-21 21:33:54 -0300

    fix: checkout

--- microsservicos/checkout/src/main/java/br/com/unicos/checkout/service/OrderPublisher.java
+++ microsservicos/checkout/src/main/java/br/com/unicos/checkout/service/OrderPublisher.java
@@ -4,10 +4,12 @@ import br.com.unicos.checkout.config.RabbitConfig;
 import br.com.unicos.checkout.dto.OrderRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service @RequiredArgsConstructor
 public class OrderPublisher {
+    @Autowired
     private RabbitTemplate template;
 
     public void publish(OrderRequest order){
